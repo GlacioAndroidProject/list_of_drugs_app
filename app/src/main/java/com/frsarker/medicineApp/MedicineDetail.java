c72b3964031bed25a46db9032ea40549e8366fe6
TruongBX <truong.buixuansw@vn-et.com>
2021-10-28 17:59:37 +0700

    Update

---
+++ app/src/main/java/com/frsarker/medicineApp/MedicineDetail.java
@@ -0,0 +1,14 @@
+package com.frsarker.medicineApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicineDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_detail);
+    }
+}
